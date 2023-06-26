0f8e6c198149b3866124828679335a6621b7c1c9
Joao Leles <jalxpinheiro@gmail.com>
2023-06-26 16:17:40 -0300

    correção de bugs

--- MiniProjetoJava-VersaoFinal/GerenciamentoPatrimonial/src/controle/ControleEmpresa.java
+++ MiniProjetoJava-VersaoFinal/GerenciamentoPatrimonial/src/controle/ControleEmpresa.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import modelo.*;
 
 public class ControleEmpresa {
-	private Empresa dados; 
+	private Empresa dados;
 	private ControleFiliais cFiliais;
 
 	public ControleEmpresa() {
@@ -14,13 +14,46 @@ public class ControleEmpresa {
 		dados.fillWithSomeData();
 		fillFiliaisWithSomeData();
 	}
-	
+
 	public void fillFiliaisWithSomeData() {
+		int i = 0;
 		for (Filial filial : dados.getFiliais()) {
-			filial.fillWithSomeData();
+			for (int l = 0; l < 5; l++) {
+				ItemDePatrimonio temporaria;
+
+				if (l == 0 && l == 3) {
+					temporaria = new Imobiliario();
+					((Imobiliario) temporaria).setTipo("Ap");
+					((Imobiliario) temporaria).setArea(100 + (l + i));
+					((Imobiliario) temporaria).setEndereco("SQN 32" + (l + i));
+				}
+
+				else if (l == 1 && l == 4) {
+					temporaria = new Utilitario();
+					((Utilitario) temporaria).setDescricao("top de bola" + (l + i));
+					((Utilitario) temporaria).setFuncao("Armazenamento" + (l + i));
+				}
+
+				else {
+					temporaria = new Veiculo();
+					((Veiculo) temporaria).setCor("azul");
+					((Veiculo) temporaria).setModelo("GT");
+					((Veiculo) temporaria).setFabricante("Nissan");
+				}
+
+				String nome = "Item" + (l + i);
+				double valor = 2000 + (l + i);
+				int quantidade = (l + i);
+
+				temporaria.setNome(nome);
+				temporaria.setValor(valor);
+				temporaria.setQuantidade(quantidade);
+
+				filial.setPatrimonios(temporaria);
+			}
+			i += 5;
 		}
 	}
-	
 
 	public boolean cadastroFilial(String f[], int pos) {
 		Filial filial = new Filial(f[0], Long.parseLong(f[1]), f[2], Long.parseLong(f[3]), f[4]);
@@ -61,26 +94,29 @@ public class ControleEmpresa {
 	 */
 
 	public int[] procurarPatrimonio(String nome) {
-        for (int i = 0; i < dados.getFiliais().size(); i++) {
-            Filial filial = dados.getFiliais().get(i);
-            ArrayList<ItemDePatrimonio> patrimonios = filial.getPatrimonios();
+		for (int i = 0; i < dados.getFiliais().size(); i++) {
+			Filial filial = dados.getFiliais().get(i);
+			ArrayList<ItemDePatrimonio> patrimonios = filial.getPatrimonios();
 
-            for (int j = 0; j < patrimonios.size(); j++) {
-                ItemDePatrimonio patrimonio = patrimonios.get(j);
+			for (int j = 0; j < patrimonios.size(); j++) {
+				ItemDePatrimonio patrimonio = patrimonios.get(j);
 
-                if (patrimonio.getNome().equals(nome)) {
-                    int[] posicoes = new int[3];
-                    posicoes[0] = i;
-                    posicoes[1] = j;
+				if (patrimonio.getNome().equals(nome)) {
+					int[] posicoes = new int[3];
+					posicoes[0] = i;
+					posicoes[1] = j;
 
-                    if (patrimonio instanceof Imobiliario) posicoes[2] = 1;
-                    if (patrimonio instanceof Utilitario) posicoes[2] = 2;
-                    if (patrimonio instanceof Veiculo) posicoes[2] = 3;
+					if (patrimonio instanceof Imobiliario)
+						posicoes[2] = 1;
+					if (patrimonio instanceof Utilitario)
+						posicoes[2] = 2;
+					if (patrimonio instanceof Veiculo)
+						posicoes[2] = 3;
 
-                    return posicoes;
-                }
-            }
-        }
-        return null;
+					return posicoes;
+				}
+			}
+		}
+		return null;
 	}
 }
